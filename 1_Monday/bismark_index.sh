503c5b5a4b46be5b4ea7b05c83cf9ca2525ba8f2
jamesord <79693476+jamesord@users.noreply.github.com>
2025-02-07 14:45:49 +0200

    Update bismark_index.sh

--- 1_Monday/bismark_index.sh
+++ 1_Monday/bismark_index.sh
@@ -2,5 +2,5 @@
 
 # index the TAIR10 genome
 mkdir bisulfite_genome_TAIR10
-cp Arabidopsis_thaliana.TAIR10.dna.toplevel.fa bisulfite_genome_TAIR10/
+cp ~/Share/1_Monday/TAIR10_genome/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa bisulfite_genome_TAIR10
 bismark_genome_preparation bisulfite_genome_TAIR10
